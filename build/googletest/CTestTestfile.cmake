76ef284a2efb31211a867a22569dd14d5004f413
luca-simei <luca.simei@mail.polimi.it>
2024-06-01 11:15:33 +0200

    Still not working

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ximus/SE4HPC/SE4HPC_project_part2/googletest
-# Build directory: /home/ximus/SE4HPC/SE4HPC_project_part2/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
